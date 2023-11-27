9033ee7fadf2175a4647fce910f21b2914c987fc
Pankaj Garkoti <garkotipankaj@gmail.com>
2023-11-27 13:30:12 +0530

    Mon Nov 27 13:30:12 IST 2023

--- lua/pankajgarkoti/plugins/blamer.lua
+++ lua/pankajgarkoti/plugins/blamer.lua
@@ -13,6 +13,6 @@
 
 vim.g.blamer_enabled = 1
 vim.g.blamer_delay = 0
-vim.g.blamer_show_in_visual_modes = 1
+vim.g.blamer_show_in_visual_modes = 0
 vim.g.blamer_relative_time = 1
 vim.g.blamer_prefix = " ||| "
