6b7d76f96610bde246a0d77ac52d31baf41d4f14
Pankaj Garkoti <garkotipankaj@gmail.com>
2024-09-22 17:39:36 +0530

    refactor keymap file

--- nvim/.config/nvim/lua/pankajgarkoti/core/keymaps.lua
+++ nvim/.config/nvim/lua/pankajgarkoti/core/keymaps.lua
@@ -1,89 +1,9 @@
 vim.g.mapleader = " "
 
-local opts = { noremap = true, silent = true }
-local keymap = vim.keymap
-
--- general keymaps
-keymap.set("i", "jk", "<ESC>")
-keymap.set("n", "<leader>nh", ":nohl<CR>")
--- keymap.set("n", "x", "_x")
-
--- increment or decrement numbers
-keymap.set("n", "<leader>+", "<C-a>")
-keymap.set("n", "<leader>-", "<C-x>")
-
--- window splits
-keymap.set("n", "<leader>sv", "<C-w>v")
-keymap.set("n", "<leader>sh", "<C-w>s")
-keymap.set("n", "<leader>se", "<C-w>=")
-keymap.set("n", "<leader>sx", ":close<CR>")
-
--- tab navigation
-keymap.set("n", "<leader>to", "<Cmd>tabnew<CR>")
-keymap.set("n", "<leader>tx", "<Cmd>bdelete<CR>")
-keymap.set("n", "<leader>tn", "<Cmd>bnext<CR>")
-keymap.set("n", "<leader>tp", "<Cmd>bprevious<CR>")
-
--- select block
-keymap.set("n", "<leader>bss", "md")
-keymap.set("n", "<leader>bse", "y'd")
-
--- plugin keymaps
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
-
--- CoC keymaps
-keymap.set("n", "<leader>gd", ":MaximizerToggle<CR>")
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
-
--- nvim tree (file explorer)
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
-
--- telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")  -- find files within current working directory, respects .gitignore
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")   -- find string in current working directory as you type
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
-keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")     -- list open buffers in current neovim instance
-keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")   -- list available help tags
-
--- telescope git
-keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>")   -- list all git commits (use <cr> to checkout) ["gc" for git commits]
-keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
-keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>")  -- list git branches (use <cr> to checkout) ["gb" for git branch]
-keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>")    -- list current changes per file with diff preview ["gs" for git status]
-
--- restart lsp server
-keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
-
--- harpoon
-keymap.set("n", "<leader>hm", ":lua require('harpoon.mark').add_file()<cr>")        -- mapping to restart lsp if necessary
-keymap.set("n", "<leader>hn", ":lua require('harpoon.ui').nav_next()<cr>")          -- mapping to restart lsp if necessary
-keymap.set("n", "<leader>hp", ":lua require('harpoon.ui').nav_prev()<cr>")          -- mapping to restart lsp if necessary
-keymap.set("n", "<leader>hh", ":lua require('harpoon.ui').toggle_quick_menu()<cr>") -- mapping to restart lsp if necessary
-keymap.set("n", "<leader>1", ":lua require('harpoon.ui').nav_file(1)<cr>")
-keymap.set("n", "<leader>2", ":lua require('harpoon.ui').nav_file(2)<cr>")
-keymap.set("n", "<leader>3", ":lua require('harpoon.ui').nav_file(3)<cr>")
-keymap.set("n", "<leader>4", ":lua require('harpoon.ui').nav_file(4)<cr>")
-keymap.set("n", "<leader>5", ":lua require('harpoon.ui').nav_file(5)<cr>")
-
-
--- datetime related mappings in insert mode
-keymap.set('i', '<C-d>', "<C-R>=strftime('%c')<CR>", opts)
-vim.keymap.set('i', '<C-d><C-f>', "<C-R>=strftime('%c')<CR>", opts)
-vim.keymap.set('i', '<C-d><C-d>', "<C-R>=strftime('%Y-%m-%d')<CR>", opts)
-vim.keymap.set('i', '<C-d><C-t>', "<C-R>=strftime('%H:%M:%S')<CR>", opts)
-vim.keymap.set('i', '<C-d><C-s>', "<C-R>=strftime('%m/%d/%y')<CR>", opts)
-vim.keymap.set('i', '<C-d><C-w>', "<C-R>=strftime('%A')<CR>", opts)
-
-
--- logging and notetaking keymaps
--- mark the task as done, if the checkboxes are of the form [x] or [X], change them with code
--- if they are of form ( ) or (x) call the neorg.qol.todo-items.todo.task-cycle function
-
+-- Logging and notetaking keymaps
 local function mark_task_done()
 	local line = vim.api.nvim_get_current_line()
-	local square_match = line:match("%[%s*%]")
+	local square_match = line:match("%[([ %x%-])%]")
 	local current_line = vim.api.nvim_get_current_line()
 
 	local markdown_state_map = {
@@ -104,21 +24,14 @@ local function mark_task_done()
 
 	for state, new_state in pairs(markdown_state_map) do
 		if square_match and square_match == state then
-			local new_line = current_line:gsub(boxed(square_match), boxed(new_state))
+			local new_line = current_line:gsub(boxed(state), boxed(new_state))
 			vim.api.nvim_set_current_line(new_line)
 		end
 	end
 end
 
-vim.keymap.set('n', '<leader><CR>', '<Plug>(neorg.qol.todo-items.todo.task-cycle)',
-	{ noremap = true, silent = true, desc = "Mark Neorg todo list item as done" }
-)
-vim.keymap.set('n', '<leader>lc', mark_task_done,
-	{ noremap = true, silent = true, desc = "Mark Markdown todo list item as done" }
-)
 
--- Function to insert timestamped line below current line.
--- same_line: boolean, if true, inserts the timestamp at the end of the current line, otherwise inserts it on the next line
+-- Helper function to insert timestamped line
 local function insert_timestamped_line(same_line)
 	local current_line = vim.fn.line('.')
 	local indent = vim.fn.indent(current_line)
@@ -126,12 +39,10 @@ local function insert_timestamped_line(same_line)
 	local current_content = vim.api.nvim_get_current_line()
 
 	if same_line then
-		-- Insert at the end of the current line
 		local new_content = current_content .. ' - ' .. '[' .. timestamp .. '] '
 		vim.api.nvim_set_current_line(new_content)
 		vim.api.nvim_win_set_cursor(0, { current_line, #new_content })
 	else
-		-- Insert on the next line
 		local new_line = string.rep(' ', indent) .. ' - ' .. '[' .. timestamp .. '] '
 		vim.api.nvim_buf_set_lines(0, current_line, current_line, false, { new_line })
 		vim.api.nvim_win_set_cursor(0, { current_line + 1, #new_line })
@@ -140,10 +51,92 @@ local function insert_timestamped_line(same_line)
 	vim.cmd('startinsert!')
 end
 
--- Set the keymaps
-vim.keymap.set('n', '<leader>ln', function() insert_timestamped_line(false) end,
-	{ noremap = true, silent = true, desc = "Insert timestamped line below" }
-)
-vim.keymap.set('n', '<leader>ll', function() insert_timestamped_line(true) end,
-	{ noremap = true, silent = true, desc = "Insert timestamp at end of current line" }
-)
+-- Set the keymaps for timestamped lines
+
+KEYMAPS = {
+	{ "i", "jk",           "<ESC>",                                              { noremap = true, silent = true, desc = "Exit insert mode" } },
+	{ "n", "<leader>nh",   ":nohl<CR>",                                          { noremap = true, silent = true, desc = "Clear search highlights" } },
+
+	-- Increment or decrement numbers
+	{ "n", "<leader>+",    "<C-a>",                                              { noremap = true, silent = true, desc = "Increment number" } },
+	{ "n", "<leader>-",    "<C-x>",                                              { noremap = true, silent = true, desc = "Decrement number" } },
+
+	-- Window management keymaps
+	{ "n", "<leader>sv",   "<C-w>v",                                             { noremap = true, silent = true, desc = "Split window vertically" } },
+	{ "n", "<leader>sh",   "<C-w>s",                                             { noremap = true, silent = true, desc = "Split window horizontally" } },
+	{ "n", "<leader>se",   "<C-w>=",                                             { noremap = true, silent = true, desc = "Equalize window dimensions" } },
+	{ "n", "<leader>sx",   ":close<CR>",                                         { noremap = true, silent = true, desc = "Close current window" } },
+
+	-- Tab navigation keymaps
+	{ "n", "<leader>to",   "<Cmd>tabnew<CR>",                                    { noremap = true, silent = true, desc = "Open new tab" } },
+	{ "n", "<leader>tx",   "<Cmd>bdelete<CR>",                                   { noremap = true, silent = true, desc = "Close buffer" } },
+	{ "n", "<leader>tn",   "<Cmd>bnext<CR>",                                     { noremap = true, silent = true, desc = "Next buffer" } },
+	{ "n", "<leader>tp",   "<Cmd>bprevious<CR>",                                 { noremap = true, silent = true, desc = "Previous buffer" } },
+
+	-- Block selection keymaps
+	{ "n", "<leader>bss",  "md",                                                 { noremap = true, silent = true, desc = "Select block start" } },
+	{ "n", "<leader>bse",  "y'd",                                                { noremap = true, silent = true, desc = "Select block end" } },
+
+	-- Plugin keymaps
+	{ "n", "<leader>sm",   ":MaximizerToggle<CR>",                               { noremap = true, silent = true, desc = "Toggle Maximizer" } },
+
+	-- LSP
+	{ "n", "<leader>gd",   "<cmd>lua vim.lsp.buf.definition()<CR>",              { noremap = true, silent = true, desc = "Go to definition" } },
+
+	-- Nvim Tree (file explorer)
+	{ "n", "<leader>e",    ":NvimTreeToggle<CR>",                                { noremap = true, silent = true, desc = "Toggle file explorer" } },
+
+	-- Telescope keymaps
+	{ "n", "<leader>ff",   "<cmd>Telescope find_files<cr>",                      { noremap = true, silent = true, desc = "Find files" } },
+	{ "n", "<leader>fs",   "<cmd>Telescope live_grep<cr>",                       { noremap = true, silent = true, desc = "Live grep" } },
+	{ "n", "<leader>fc",   "<cmd>Telescope grep_string<cr>",                     { noremap = true, silent = true, desc = "Grep string under cursor" } },
+	{ "n", "<leader>fb",   "<cmd>Telescope buffers<cr>",                         { noremap = true, silent = true, desc = "List open buffers" } },
+	{ "n", "<leader>fh",   "<cmd>Telescope help_tags<cr>",                       { noremap = true, silent = true, desc = "List help tags" } },
+
+	-- Telescope git keymaps
+	{ "n", "<leader>gc",   "<cmd>Telescope git_commits<cr>",                     { noremap = true, silent = true, desc = "List all git commits" } },
+	{ "n", "<leader>gfc",  "<cmd>Telescope git_bcommits<cr>",                    { noremap = true, silent = true, desc = "Git commits for current file" } },
+	{ "n", "<leader>gb",   "<cmd>Telescope git_branches<cr>",                    { noremap = true, silent = true, desc = "List git branches" } },
+	{ "n", "<leader>gs",   "<cmd>Telescope git_status<cr>",                      { noremap = true, silent = true, desc = "Git status" } },
+
+	-- Restart LSP server
+	{ "n", "<leader>rs",   ":LspRestart<CR>",                                    { noremap = true, silent = true, desc = "Restart LSP server" } },
+
+	-- Harpoon keymaps
+	{ "n", "<leader>hm",   ":lua require('harpoon.mark').add_file()<CR>",        { noremap = true, silent = true, desc = "Add file to Harpoon" } },
+	{ "n", "<leader>hn",   ":lua require('harpoon.ui').nav_next()<CR>",          { noremap = true, silent = true, desc = "Next Harpoon mark" } },
+	{ "n", "<leader>hp",   ":lua require('harpoon.ui').nav_prev()<CR>",          { noremap = true, silent = true, desc = "Previous Harpoon mark" } },
+	{ "n", "<leader>hh",   ":lua require('harpoon.ui').toggle_quick_menu()<CR>", { noremap = true, silent = true, desc = "Toggle Harpoon quick menu" } },
+	{ "n", "<leader>1",    ":lua require('harpoon.ui').nav_file(1)<CR>",         { noremap = true, silent = true, desc = "Harpoon file 1" } },
+	{ "n", "<leader>2",    ":lua require('harpoon.ui').nav_file(2)<CR>",         { noremap = true, silent = true, desc = "Harpoon file 2" } },
+	{ "n", "<leader>3",    ":lua require('harpoon.ui').nav_file(3)<CR>",         { noremap = true, silent = true, desc = "Harpoon file 3" } },
+	{ "n", "<leader>4",    ":lua require('harpoon.ui').nav_file(4)<CR>",         { noremap = true, silent = true, desc = "Harpoon file 4" } },
+	{ "n", "<leader>5",    ":lua require('harpoon.ui').nav_file(5)<CR>",         { noremap = true, silent = true, desc = "Harpoon file 5" } },
+
+	-- Datetime related keymaps in insert mode
+	{ 'i', '<C-d>',        "<C-R>=strftime('%c')<CR>",                           { map = true, noremap = false, silent = true, desc = "Insert current datetime" } },
+	{ 'i', '<C-d><C-f>',   "<C-R>=strftime('%c')<CR>",                           { noremap = true, silent = true, desc = "Insert full datetime" } },
+	{ 'i', '<C-d><C-d>',   "<C-R>=strftime('%Y-%m-%d')<CR>",                     { noremap = true, silent = true, desc = "Insert current date" } },
+	{ 'i', '<C-d><C-t>',   "<C-R>=strftime('%H:%M:%S')<CR>",                     { noremap = true, silent = true, desc = "Insert current time" } },
+	{ 'i', '<C-d><C-s>',   "<C-R>=strftime('%m/%d/%y')<CR>",                     { noremap = true, silent = true, desc = "Insert short date" } },
+	-- { 'i', '<C-d><C-w>',   "<C-R>=strftime('%A')<CR>",                           { noremap = true, silent = true, desc = "Insert current weekday" } },
+	{ 'i', '<C-d><C-w>',   "<C-R>=strftime('%A %Y-%m-%d')<CR>",                  { noremap = true, silent = true, desc = "Insert current weekday" } },
+	{ 'n', '<leader><CR>', '<Plug>(neorg.qol.todo-items.todo.task-cycle)',       { noremap = true, silent = true, desc = "Mark Neorg todo list item as done" } },
+	{ 'n', '<leader>lc',   mark_task_done,                                       { noremap = true, silent = true, desc = "Mark Markdown todo list item as done" } },
+	{ 'n', '<leader>ln',   function() insert_timestamped_line(false) end,        { noremap = true, silent = true, desc = "Insert timestamped line below" } },
+	{ 'n', '<leader>ll',   function() insert_timestamped_line(true) end,         { noremap = true, silent = true, desc = "Insert timestamp at end of current line" } },
+}
+
+
+
+local init_keymaps = function()
+	for _, keymap in pairs(KEYMAPS) do
+		local mode = keymap[1]
+		local key = keymap[2]
+		local cmd = keymap[3]
+		local opts = keymap[4]
+		vim.keymap.set(mode, key, cmd, opts)
+	end
+end
+
+init_keymaps()
